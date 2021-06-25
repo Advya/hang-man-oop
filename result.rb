62be85cb3a1ab2da810bc66316e2f3dba8075e47
advya <50411324+Advya@users.noreply.github.com>
2021-06-25 17:04:03 +0300

    done visual

--- result.rb
+++ result.rb
@@ -3,6 +3,9 @@ class Result
 		cls
 		puts "Word: " + get_word_for_print(game.letters, game.good_letters)
 		puts "Errors: #{game.errors} #{game.bad_letters.join(", ")}"
+
+		print_hang(game.errors)
+
 		if game.errors >= 7
 			puts "You lose!"
 		else
@@ -28,6 +31,127 @@ class Result
 	end
 	def cls
 		system "cls"
+	end
+
+	def print_hang(errors)
+		case errors
+			when 0
+				 puts '
+		          _______
+		          |/
+		          |
+		          |
+		          |
+		          |
+		          |
+		          |
+		          |
+		        __|________
+		        |         |
+		        '
+			when 1
+				puts '
+		        _______
+		        |/
+		        |     ( )
+		        |
+		        |
+		        |
+		        |
+		        |
+		        |
+		      __|________
+		      |         |
+		      '
+			when 2
+				      puts '
+		        _______
+		        |/
+		        |     ( )
+		        |      |
+		        |
+		        |
+		        |
+		        |
+		        |
+		      __|________
+		      |         |
+		      '
+
+			when 3
+		      puts '
+		        _______
+		        |/
+		        |     ( )
+		        |      |_
+		        |        \\
+		        |
+		        |
+		        |
+		        |
+		      __|________
+		      |         |
+		      '
+		    when 4
+		      puts '
+		        _______
+		        |/
+		        |     ( )
+		        |     _|_
+		        |    /   \\
+		        |
+		        |
+		        |
+		        |
+		      __|________
+		      |         |
+		      '
+		    when 5
+		      puts '
+		        _______
+		        |/
+		        |     ( )
+		        |     _|_
+		        |    / | \\
+		        |      |
+		        |
+		        |
+		        |
+		      __|________
+		      |         |
+		      '
+
+		    when 6
+		      puts '
+		        _______
+		        |/
+		        |     ( )
+		        |     _|_
+		        |    / | \\
+		        |      |
+		        |     / \\
+		        |    /   \\
+		        |
+		      __|________
+		      |         |
+		      '
+		    when 7
+		      puts '
+		        _______
+		        |/     |
+		        |     (_)
+		        |     _|_
+		        |    / | \\
+		        |      |
+		        |     / \\
+		        |    /   \\
+		        |
+		      __|________
+		      |         |
+		      '
+		    end
+		    
+
 	end
 
 end
